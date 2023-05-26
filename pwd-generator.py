6d9834574a8fc603700d898aeca02635bb513c90
PythonCoder8 <72826534+PythonCoder8@users.noreply.github.com>
2023-05-26 16:15:21 -0700

    Update pwd-generator.py

--- pwd-generator.py
+++ pwd-generator.py
@@ -3,23 +3,9 @@ import string, random, sys
 special_chars = "!@#$%^&*()"
 
 try:
-    chars = int(
-        input(
-            "How many characters do you want to have in your new password? (a strong password should have at least 12)?: "
-        )
-    )
+    chars = int(input("How many characters do you want to have in your new password? (a strong password should have at least 12)?: "))
 
-except:
+except ValueError:
     sys.exit("Input should be a number")
 
-print(
-    "".join(
-        random.choices(
-            string.ascii_uppercase
-            + string.ascii_lowercase
-            + string.digits
-            + special_chars,
-            k=chars,
-        )
-    )
-)
+print("".join(random.choices(string.ascii_uppercase + string.ascii_lowercase + string.digits + special_chars, k=chars)))
